a700561d01d5ef9bfc8fdce3f416849cf2383e4d
Orlando <53872517+orlando1424@users.noreply.github.com>
2019-11-20 09:24:46 -0700

    starter

---
+++ ComputerAssistedInstruction/ComputerAssistedInstruction/ComputerAssistedInstruction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ComputerAssistedInstruction
+{
+    class ComputerAssistedInstruction
+    {
+        
+        static void Main(string[] args)
+        {
+            Random randomNumbers = new Random();
+
+            int firstNumber = randomNumbers.Next(10);
+            int secondNumber = randomNumbers.Next(10);
+
+        }
+    }
+}
