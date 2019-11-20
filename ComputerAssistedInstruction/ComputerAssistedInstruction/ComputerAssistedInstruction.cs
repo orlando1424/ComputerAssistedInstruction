b53823caf617e9d6160fb4a6f0f2ab73c7bfa625
Orlando <53872517+orlando1424@users.noreply.github.com>
2019-11-20 09:55:17 -0700

    added checkAnswer method

--- ComputerAssistedInstruction/ComputerAssistedInstruction/ComputerAssistedInstruction.cs
+++ ComputerAssistedInstruction/ComputerAssistedInstruction/ComputerAssistedInstruction.cs
@@ -13,6 +13,23 @@ namespace ComputerAssistedInstruction
             int firstNumber = randomNumbers.Next(10);
             int secondNumber = randomNumbers.Next(10);
 
+            Console.WriteLine($"{firstNumber}  {secondNumber}");
+
+            int answer = 0;
+            int product = firstNumber * secondNumber;
+            bool isCorrect = false;
+            problemGeneration(firstNumber, secondNumber, answer);
+     
+        }
+
+        static void problemGeneration(int num1, int num2, int answer)
+        {
+            Console.WriteLine($"How much is {num1} times {num2}");
+        }
+
+        static void checkAnswer(int answer, int product, bool isCorrect)
+        {
+            
         }
     }
 }
